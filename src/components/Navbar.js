5fad6069aa248728f93298df81cb1a8d60086492
Arghya Dutta <102239340+arghyadutta080@users.noreply.github.com>
2022-11-17 12:01:58 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ function Navbar() {
                         <div className="collapse navbar-collapse" id="navbarCollapse">
                             <ul className="navbar-nav me-auto mb-2 mb-md-0">
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/">Home</Link>
+                                    <Link className="nav-link" to="/FitnFine">Home</Link>
                                 </li>
                                 <li className="nav-item">
                                     <Link className="nav-link" to="/book">Book your slot</Link>
